e3377c352a8161fc70258e00699b59d272ed3503
Echedey Aguilar Hernández <eaguilar1@us.es>
2026-01-27 11:09:01 +0100

    feat: add Pex1202L sensor reader factory function declaration.

--- ecu_atc8110/adquisicion_datos/pex1202l.hpp
+++ ecu_atc8110/adquisicion_datos/pex1202l.hpp
@@ -1,36 +1,12 @@
 #pragma once
 
-#include <vector>
-#include <string>
+#include "sensor_config.hpp"
+#include "../common/interfaces.hpp"
+
 #include <memory>
-
-#include "common/analog_sample.hpp"
-#include "common/isensor_reader.hpp"
-
-// =============================
-// DEFINICIÓN BRAKE SWITCH (IXPIO)
-// =============================
-#define BRAKE_SW_BIT   0
-#define BRAKE_SW_MASK (1u << BRAKE_SW_BIT)
 
 namespace adquisicion_datos {
 
-class Pex1202L : public common::ISensorReader {
-public:
-    explicit Pex1202L(SensorConfig config);
-    ~Pex1202L() override;
-
-    bool start() override;
-    void stop() override;
-
-    std::vector<common::AnalogSample> read_samples() override;
-
-private:
-    SensorConfig config_;
-    bool mock_mode_{false};
-};
-
-std::unique_ptr<common::ISensorReader>
-CreatePex1202L(SensorConfig config);
+std::unique_ptr<common::ISensorReader> CreatePex1202L(SensorConfig config);
 
 } // namespace adquisicion_datos
